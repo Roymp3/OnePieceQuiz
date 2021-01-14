fc0a81ef0a254cbbbb364836c660d57da2cf4da9
Roymp3 <71888012+Roymp3@users.noreply.github.com>
2021-01-14 17:32:35 -0300

    Add files via upload

--- jogo.js
+++ jogo.js
@@ -14,134 +14,176 @@ function resposta1(){
      resp[10] = document.getElementById("pergunta30").checked
      resp[11] = document.getElementById("pergunta33").checked
     let score = new Array(12);
-    let  soma = new Array(2);
+    let  soma;
+
     if(resp[0] == true ){
-
-       score[0] = 10;
-
+      score[0] = 10;
+       document.getElementById("resposta1").style.color = "green"
+       document.getElementById("resposta1").textContent = "Acertou !!"
+     
     }
     else{
       score[0] = 0;
+
+      document.getElementById("resposta1").style.color = "red"
+      document.getElementById("resposta1").textContent = "Errou !!"
     }
 
     if(resp[1] == true ){
         score[1] = 10;
+        document.getElementById("resposta2").style.color = "green"
+        document.getElementById("resposta2").textContent = "Acertou !!"
 
      }
      else{
        score[1] = 0;
+       document.getElementById("resposta2").style.color = "red"
+       document.getElementById("resposta2").textContent = "Errou !!"
      }
 
      if(resp[2] == true ){
 
         score[2]= 10;
+        document.getElementById("resposta3").style.color = "green"
+        document.getElementById("resposta3").textContent = "Acertou !!"
 
      }
      else{
        score[2] = 0;
+       document.getElementById("resposta3").style.color = "red"
+       document.getElementById("resposta3").textContent = "Errou !!"
      }
 
      if(resp[3] == true ){
-
       score[3]= 10;
+      document.getElementById("resposta4").style.color = "green"
+      document.getElementById("resposta4").textContent = "Acertou !!"
 
      }
      else{
      score[3] = 0;
+     document.getElementById("resposta4").style.color = "red"
+     document.getElementById("resposta4").textContent = "Errou !!"
      }
 
      if(resp[4] == true ){
 
       score[4]= 10;
+      document.getElementById("resposta5").style.color = "green"
+      document.getElementById("resposta5").textContent = "Acertou !!"
 
    }
    else{
      score[4] = 0;
+     document.getElementById("resposta5").style.color = "red"
+     document.getElementById("resposta5").textContent = "Errou !!"
    }
 
    if(resp[5] == true ){
 
     score[5]= 10;
+    document.getElementById("resposta6").style.color = "green"
+    document.getElementById("resposta6").textContent = "Acertou !!"
 
    }
   else{
    score[5] = 0;
+   document.getElementById("resposta6").style.color = "red"
+   document.getElementById("resposta6").textContent = "Errou !!"
   }
 
   if(resp[6] == true ){
 
     score[6]= 10;
+    document.getElementById("resposta7").style.color = "green"
+    document.getElementById("resposta7").textContent = "Acertou !!"
 
  }
  else{
    score[6] = 0;
+   document.getElementById("resposta7").style.color = "red"
+   document.getElementById("resposta7").textContent = "Errou !!"
  }
 
  if(resp[7] == true ){
 
   score[7]= 10;
+  document.getElementById("resposta8").style.color = "green"
+  document.getElementById("resposta8").textContent = "Acertou !!"
 
 }
 else{
  score[7] = 0;
+ document.getElementById("resposta8").style.color = "red"
+ document.getElementById("resposta8").textContent = "Errou !!"
 }
 
 if(resp[8] == true ){
 
   score[8]= 10;
+  document.getElementById("resposta9").style.color = "green"
+  document.getElementById("resposta9").textContent = "Acertou !!"
 
 }
 else{
  score[8] = 0;
+ document.getElementById("resposta9").style.color = "red"
+ document.getElementById("resposta9").textContent = "Errou !!"
 }
 
 if(resp[9] == true ){
 
   score[9]= 10;
+  document.getElementById("resposta10").style.color = "green"
+  document.getElementById("resposta10").textContent = "Acertou !!"
 
 }
 else{
  score[9] = 0;
+ document.getElementById("resposta10").style.color = "red"
+ document.getElementById("resposta10").textContent = "Errou !!"
 }
 
 if(resp[10] == true ){
 
   score[10]= 10;
+  document.getElementById("resposta11").style.color = "green"
+  document.getElementById("resposta11").textContent = "Acertou !!"
 
 }
 else{
  score[10] = 0;
+ document.getElementById("resposta11").style.color = "red"
+ document.getElementById("resposta11").textContent = "Errou !!"
 }
 
 if(resp[11] == true ){
 
   score[11]= 10;
+  document.getElementById("resposta12").style.color = "green"
+  document.getElementById("resposta12").textContent = "Acertou !!"
 
 }
 else{
  score[11] = 0;
+ document.getElementById("resposta12").style.color = "red"
+ document.getElementById("resposta12").textContent = "Errou !!"
 }
 
 
-  soma[0] = score[0] + score[1] + score[2] + score[3] + score[4] + score[5] + score[6] + score[7] + score[8] + score[9] + score[10] + score[11];
-  soma[1] = soma[0]
-  localStorage.setItem("soma", soma[0])
-  localStorage.setItem("record",soma[1])
-  let consulta = localStorage.getItem("soma");
-  let recorde = localStorage.getItem("record")
+soma = score[0] + score[1] + score[2] + score[3] + score[4] + score[5] + score[6] + score[7] + score[8] + score[9] + score[10] + score[11];
+localStorage.setItem("soma", soma)
+let a = localStorage.getItem("soma");
 
- if(recorde > consulta){
-  localStorage.setItem("record",soma[1])
-  document.getElementById("saida2").textContent = recorde
-  document.getElementById("saida").textContent = consulta
+if(localStorage.getItem("soma") > a){
 
- }
- else{
-  document.getElementById("saida2").textContent = consulta
-  document.getElementById("saida").textContent = consulta
- }
+  document.getElementById("saida").textContent = localStorage.getItem("soma")
+  document.textContent("saida2").textContent = localStorage.getItem("soma")
+}
+else{
 
-
-  console.log("função 1")
+  document.getElementById("saida").textContent = localStorage.getItem("soma")
+  document.getElementById("saida2").textContent = a;
+}
 
 }
